154e328fb79a066b5db1f0b84562c267e935c732
elzayts <40432959+elzayts@users.noreply.github.com>
2019-05-09 23:38:57 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,27 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
+from sqlalchemy import create_engine,
+from DB_get import get_rows
 app=Flask(__name__)
 
 filt = {}
+db = create_engine('postgresql+psycopg2://postgres:tiger@localhost/test')
 
-@app.route("/main", methods=['GET', 'POST'])
+@app.route("/index1", methods=['GET', 'POST'])
 def main():
     if request.method == 'POST':
-        filt['street'] = request.form['street']
         filt['room'] = request.form['rooms']
-        # filt[distr]=request.form['district']
+        filt['distr']=request.form['district']
         # filt[t_area] = request.form['total_area']
         # filt[l_area] = request.form['living_area']
         # filt[residence]= request.form['residence_type']
+        r = get_rows(filt['room'])
         return redirect(url_for('show'))
     else:
       return render_template("index1.html")
 
 
-@app.route("/results", methods=['GET'])
+@app.route("/info", methods=['GET'])
 def show():
-    #price will be returned after processing data from db
-    #another info will be shown on the page as well
-    return render_template("results.html", p=price)
-
-
-
+    return render_template("info.html")
